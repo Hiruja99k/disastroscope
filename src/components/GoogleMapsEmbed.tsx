afe31f8f1c2ab88913230472f6f9e86808b768f0
Hiruja99k <hiruja99k@gmail.com>
2025-08-23 22:39:38 +0530

    Fix map size issue: Replace small Google Maps embed with larger 1800px height version for better visibility in dashboard and maps tab

--- src/components/GoogleMapsEmbed.tsx
+++ src/components/GoogleMapsEmbed.tsx
@@ -10,30 +10,33 @@ interface GoogleMapsEmbedProps {
 }
 
 export default function GoogleMapsEmbed({ 
-  width = '600px', 
-  height = '400px', 
+  width = '100%', 
+  height = '1800px', 
   location = '',
   zoom = 14,
   mapType = 'm',
   className = ''
 }: GoogleMapsEmbedProps) {
-  const mapUrl = `https://maps.google.com/maps?width=${width}&height=${height}&hl=en&q=${encodeURIComponent(location)}&t=${mapType}&z=${zoom}&ie=UTF8&iwloc=B&output=embed`;
-
   return (
-    <div className={`mapouter ${className}`} style={{ position: 'relative', textAlign: 'right', width, height }}>
-      <div className="gmap_canvas" style={{ overflow: 'hidden', background: 'none!important', width, height }}>
+    <div className={`mapouter ${className}`}>
+      <div className="gmap_canvas">
         <iframe 
           className="gmap_iframe" 
+          width="100%" 
           frameBorder="0" 
           scrolling="no" 
           marginHeight={0} 
           marginWidth={0} 
-          src={mapUrl}
-          style={{ width: `${width}!important`, height: `${height}!important` }}
+          src="https://maps.google.com/maps?width=1800&amp;height=1800&amp;hl=en&amp;q=&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
           title="Google Maps"
         />
+        <a href="https://embed-googlemap.com">embed-googlemap.com</a>
       </div>
-      <a href="https://embed-googlemap.com">google maps embed</a>
+      <style>{`
+        .mapouter{position:relative;text-align:right;width:100%;height:1800px;}
+        .gmap_canvas {overflow:hidden;background:none!important;width:100%;height:1800px;}
+        .gmap_iframe {height:1800px!important;}
+      `}</style>
     </div>
   );
 }
